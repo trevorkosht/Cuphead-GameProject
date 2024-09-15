0b365c6073f0ab5f49661e25c35586dc08538055
bvidmarmcewen <benvidmarmcewen@gmail.com>
2024-09-15 18:47:29 -0400

    Player animation infrastructure, added: - IAnimation interface - beginnings of PlayerJumpAnimation class - player jump animation sprites

---
+++ Sprint0/Sprint0/IAnimation.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public interface IAnimation {
+    static int updatesPerFrame;
+    void load(ContentManager content);
+    void update(GameTime gameTime, int x, int y);
+    void draw(SpriteBatch spriteBatch);
+}
---
+++ Sprint0/Sprint0/PlayerJumpAnimation.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class PlayerJumpAnimation : IAnimation {
+    private Texture2D texture;
+    private static int updatesPerFrame = 5;
+    private static int frameCount = 8;
+    public SpriteBatch _spriteBatch;
+    public List<Texture2D> animationFrames;
+    private int updateCount = 0;
+    private int currentFrame = 0;
+    private int xPos;
+    private int yPos;
+
+    public void load(ContentManager content) {
+        //this.texture = content.Load<Texture2D>("ch-jump");
+
+        //Adding each frame of jump animation
+        animationFrames.Add(content.Load<Texture2D>("PlayerJumpTextures/cuphead_jump_0001"));
+        animationFrames.Add(content.Load<Texture2D>("PlayerJumpTextures/cuphead_jump_0002"));
+        animationFrames.Add(content.Load<Texture2D>("PlayerJumpTextures/cuphead_jump_0003"));
+        animationFrames.Add(content.Load<Texture2D>("PlayerJumpTextures/cuphead_jump_0004"));
+        animationFrames.Add(content.Load<Texture2D>("PlayerJumpTextures/cuphead_jump_0005"));
+        animationFrames.Add(content.Load<Texture2D>("PlayerJumpTextures/cuphead_jump_0006"));
+        animationFrames.Add(content.Load<Texture2D>("PlayerJumpTextures/cuphead_jump_0007"));
+        animationFrames.Add(content.Load<Texture2D>("PlayerJumpTextures/cuphead_jump_0008"));
+    }
+
+    public void update(GameTime gameTime, int x, int y) {
+        this.xPos = x;
+        this.yPos = y;
+
+        //Handles animation speed by only updating displayed frame every updatesPerFrame game updates
+        if (updateCount >= updatesPerFrame) {
+            updateCount = 0;
+
+            if(currentFrame + 1 < frameCount) {
+                currentFrame++;
+            }
+            else {
+                currentFrame = 0;
+            }
+        }
+        else {
+            updateCount++;
+        }
+    }
+    public void draw(SpriteBatch spriteBatch) {
+
+    }
+}
