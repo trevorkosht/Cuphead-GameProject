de848f3bdb2c1af53a2ed5844244a2fe27ca44f1
bvidmarmcewen <benvidmarmcewen@gmail.com>
2024-09-21 14:29:33 -0400

    Renamed health component file to be more accurate

--- Sprint0/Sprint0/GameObject/HealthComponent.cs
+++ Sprint0/Sprint0/GameObject/HealthComponent.cs
@@ -1,14 +1,14 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-public class HealthController : IComponent {
+public class HealthComponent : IComponent {
     public GameObject GameObject {  get; set; }
     public bool enabled { get; set; }
     public int maxHealth { get; set; }
     public int currentHealth { get; set; }
     public bool isDead { get; set; }
 
-    public HealthController(GameObject gameObject, int maxHealth, int currentHealth, bool isDead) {
+    public HealthComponent(GameObject gameObject, int maxHealth, int currentHealth, bool isDead) {
         GameObject = gameObject;
         this.maxHealth = maxHealth;
         this.currentHealth = currentHealth;
