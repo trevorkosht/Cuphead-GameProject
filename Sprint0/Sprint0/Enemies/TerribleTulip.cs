48c930beee8cb58897c6a1e35cf7a04acf6486ec
bvidmarmcewen <benvidmarmcewen@gmail.com>
2024-09-29 17:59:59 -0400

    Minor bit of logic to make the tulip projectiles spin the right way relative to their motion.

--- Sprint0/Sprint0/Enemies/TerribleTulip.cs
+++ Sprint0/Sprint0/Enemies/TerribleTulip.cs
@@ -7,6 +7,7 @@ public class TerribleTulip : BaseEnemy
 {
     private double shootCooldown;
     private Texture2D projectileTexture;        // Store the projectile texture
+    private List<GameObject> projectiles = new List<GameObject>();
 
     public override void Initialize(Texture2D texture, Texture2DStorage storage)
     {
@@ -29,7 +30,8 @@ public class TerribleTulip : BaseEnemy
             Vector2 playerPosition = new Vector2(player.X, player.Y);
             // Create and shoot a homing projectile towards the player
             GameObject projectile = new GameObject(GameObject.X, GameObject.Y, new HomingProjectile(GameObject.position));
-            SpriteRenderer projSrend = new SpriteRenderer(new Rectangle(projectile.X, projectile.Y, 144, 144), false);
+            projectiles.Add(projectile);
+            SpriteRenderer projSrend = new SpriteRenderer(new Rectangle(projectile.X, projectile.Y, 144, 144), true);
             projectile.AddComponent(projSrend);
             projSrend.spriteScale = .5f;
             projSrend.addAnimation("spin", new Animation(projectileTexture, 3, 12, 144, 144));
@@ -39,11 +41,27 @@ public class TerribleTulip : BaseEnemy
         }
     }
 
-    public override void Update(GameTime gameTime)
-    {
+    public override void Update(GameTime gameTime) {
         base.Update(gameTime);
 
+        //Logic to ensure the projectile is spinning in the proper direction relative to its motion
+        int i = 0;
+        while (i < projectiles.Count) {
+            if (projectiles[i].GetComponent<SpriteRenderer>() == null) {
+                projectiles.Remove(projectiles[i]);
+            }
+            else {
+                if (projectiles[i].X > player.X) {
+                    projectiles[i].GetComponent<SpriteRenderer>().isFacingRight = false;
+                }
+                else {
+                    projectiles[i].GetComponent<SpriteRenderer>().isFacingRight = true;
+                }
+                i++;
+            }
+        }
         // Shoot logic handled in the Shoot method
         Shoot(gameTime);
     }
 }
+
