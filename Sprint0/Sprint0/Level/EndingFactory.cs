6b71c8e1f9e2b89b4b31e93e9d2a6c9943639499
Zol Chen <chen.11432@buckeyemail.osu.edu>
2024-11-03 15:27:30 -0500

    added ending factory

---
+++ Sprint0/Sprint0/Level/EndingFactory.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+
+public static class EndingFactory
+{
+    private static Dictionary<string, (int width, int height, float orderInLayer)> EndSizes = new Dictionary<string, (int width, int height, float orderInLayer)>()
+    {
+            { "DeathMessage", (500, 546, 0.91f) },
+            { "WinScreenBackground", (526, 306, 0.001f) },
+            { "WinScreenBoard", (980, 760, 0.002f) },
+            { "WinScreenResultsText", (902, 710, 0.002f) },
+            { "WinScreenCuphead", (273, 476, 0.002f) },
+            { "WinScreenUnearnedStar", (650, 472, 0.002f) },
+            { "WinScreenStarAppearAnimation", (1564, 458, 0.003f) },
+            { "WinScreenLine", (642, 270, 0.003f) },
+            { "WinScreenStar", (970, 682, 0.004f) },
+            { "WinScreenCircle", (668, 522, 0.009f) },
+    };
+
+    public static GameObject CreateElement(string subtype, Vector2 position)
+    {
+        Texture2D texture = GOManager.Instance.textureStorage.GetTexture(subtype);
+
+        if (EndSizes.TryGetValue(subtype, out (int width, int height, float orderInLayer) endData))
+        {
+            Rectangle destRectangle = new Rectangle((int)position.X, (int)position.Y, endData.width, endData.height);
+            GameObject EndScreen = new GameObject(destRectangle.X, destRectangle.Y);
+            SpriteRenderer spriteRenderer = new SpriteRenderer(destRectangle, true);
+            Animation backgroundTexture = new Animation(texture, 1, 1, destRectangle.Height, destRectangle.Width);
+
+            spriteRenderer.addAnimation("end", backgroundTexture);
+            spriteRenderer.setAnimation("end");
+            EndScreen.AddComponent(spriteRenderer);
+            return EndScreen;
+        }
+        else
+        {
+            Console.WriteLine($"Unknown background subtype: {subtype}");
+            return null;
+        }
+    }
+
+}
